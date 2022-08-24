b7f005a5a212fe889bb9752825e07f70eef56d30
Michael Chan <michaelcychan@gmail.com>
2022-08-24 21:05:11 +0100

    return req now in desc order of likes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,17 +6,21 @@ class ApiController < ApplicationController
   end
 
   def show
-    puts params[:tag]
+
     parameters = ""
     if params[:tag]
       parameters += "tag=" + params[:tag]
     end
-    puts parameters
 
     @response = RestClient.get "https://api.hatchways.io/assessment/blog/posts?#{parameters}", {content_type: :json, accept: :json}
-    
-
     @posts = JSON.parse(@response.body)["posts"]
+    # Now @posts is an array of hash
+    
+    @posts.each do |post|
+      puts "#{post}\n"
+    end
+
+    @posts.sort_by! {|post| post["likes"] }.reverse!
 
     render json: @posts
   end
